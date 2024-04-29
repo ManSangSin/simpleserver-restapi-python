582f63d799f060d1072e4c7297e4662a538d5b58
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-04-29 23:10:01 +0100

    import requests module and create "/tv_shows" endpoint
    endpoint calls tvmaze api and returns all tv shows data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
@@ -10,3 +11,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/tv_shows")
+def get_shows():
+    api_url = "https://api.tvmaze.com/shows"
+    tv_shows_data = requests.get(api_url).json()
+    return tv_shows_data
